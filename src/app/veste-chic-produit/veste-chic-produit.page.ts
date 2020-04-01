26e525820a888ce653c5f10294382b066e9d835b
Alain Bouquety <abouquety1@gmail.com>
2020-04-01 18:51:09 +0200

    Go back

--- src/app/veste-chic-produit/veste-chic-produit.page.ts
+++ src/app/veste-chic-produit/veste-chic-produit.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-veste-chic-produit',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VesteChicProduitPage implements OnInit {
 
-  constructor() { }
+  constructor(private location: Location) { }
 
   ngOnInit() {
   }
-
+  goBack(){
+    this.location.back();
+  }
 }
