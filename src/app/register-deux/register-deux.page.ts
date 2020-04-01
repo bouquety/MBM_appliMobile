87d4d758846297b539af9593d387ce3167ce3469
Alain Bouquety <abouquety1@gmail.com>
2020-04-01 19:03:46 +0200

    Correction goBack

--- src/app/register-deux/register-deux.page.ts
+++ src/app/register-deux/register-deux.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-register-deux',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterDeuxPage implements OnInit {
 
-  constructor() { }
+  constructor( private location: Location) { }
 
   ngOnInit() {
   }
-
+  goBack(){
+    this.location.back();
+  }
 }
--- src/app/register-quatre/register-quatre.page.ts
+++ src/app/register-quatre/register-quatre.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-register-quatre',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterQuatrePage implements OnInit {
 
-  constructor() { }
+  constructor( private location: Location) { }
 
   ngOnInit() {
   }
-
+  goBack(){
+    this.location.back();
+  }
 }
