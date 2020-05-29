521efd2f26c4ff948a46fab940cc88429d5623f9
Alain Bouquety <abouquety1@gmail.com>
2020-05-29 17:41:45 +0200

    Ajustement jalon 2

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,11 +9,11 @@ import { Location } from '@angular/common';
 })
 export class LoginPage implements OnInit {
 
-  constructor( private location: Location) { }
+  constructor( private location: Location, private router: Router) { }
 
   ngOnInit() {
   }
   goBack(){
-    this.location.back();
+    this.router.navigateByUrl('/home');
   }
 }
