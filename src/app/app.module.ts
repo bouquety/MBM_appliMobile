e72b761fe8cbb7cc5b5ae2f944db665a5e194dae
Alain Bouquety <abouquety1@gmail.com>
2020-05-29 22:45:55 +0200

    page regieP graph sportwear

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import 'chart.js';
 
 @NgModule({
   declarations: [AppComponent],
