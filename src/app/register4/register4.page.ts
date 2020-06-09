ff5c89b94de19a4ddf0ff89fb462e978be416410
Alexandre M <mohamed.alexandre@yahoo.fr>
2020-06-09 11:48:10 +0200

    add register 5 and fix register

--- src/app/register4/register4.page.ts
+++ src/app/register4/register4.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-register4',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Register4Page implements OnInit {
 
-  constructor() { }
-
+  constructor(private navController: NavController) { }
   ngOnInit() {
   }
 
+  doRegisterEnd(form): void {
+    this.navController.navigateForward('home');
+  }
+  goRegister5(form): void {
+    this.navController.navigateForward('register5');
+  }
 }
